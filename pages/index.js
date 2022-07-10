3ce0d80bcdf00e21c427aab7b73eef8cfacc2ed7
Jesmaster <jesmasterha@gmail.com>
2022-07-10 01:11:58 -0400

    cache query results with no filters

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useEffect, useMemo, useRef, useState } from "react"
 import Skill from '../components/Skill'
@@ -39,7 +38,6 @@ export default function Home(props) {
 
   useEffect(() => {
     if (isValidating === false && data) {
-      console.log('updating...');
       setFilteredSkills(data);
     }
   }, [data, isValidating]);
