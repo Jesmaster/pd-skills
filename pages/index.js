7e85272e952afaadb6fdd27f8c7292fcb30c475d
Jesmaster <jesmasterha@gmail.com>
2022-07-14 20:06:35 -0400

    added localStorage cache layer

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,50 @@ import useSWR from 'swr'
 import QueryString from 'qs'
 import { getSkills } from '../lib/query'
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
+//https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+const storageAvailable = (type) => {
+  let storage;
+  try {
+      storage = window[type];
+      const x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+  }
+  catch(e) {
+      return e instanceof DOMException && (
+          // everything except Firefox
+          e.code === 22 ||
+          // Firefox
+          e.code === 1014 ||
+          // test name field too, because code might not be present
+          // everything except Firefox
+          e.name === 'QuotaExceededError' ||
+          // Firefox
+          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          // acknowledge QuotaExceededError only if there's something already stored
+          (storage && storage.length !== 0);
+  }
+}
+
+const localAvailable = storageAvailable('localStorage');
+
+const fetcher = async url => {
+  
+  if (localAvailable && localStorage.getItem(url)) {
+    return JSON.parse(localStorage.getItem(url));
+  }
+  else {
+    const res = await fetch(url);
+    const result = await res.json();
+
+    if (localAvailable) {
+      localStorage.setItem(url, JSON.stringify(result));
+    }
+
+    return result;
+  }
+};
 
 export default function Home(props) {
   const { skills, allSchoolFilters, allTypeFilters, allDistanceFilters, allCosts, allStr, allKeywords, allVelocity, allHoming, allRecovery } = props;
